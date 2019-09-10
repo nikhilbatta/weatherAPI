cdab8f12a0fd5e0280f54ae75f5cd19767ed9a96
nikhlbatta <nbatta08@gmail.com>
2019-09-10 16:32:54 -0700

    weatherAPI with GIFs(still needs work)

--- src/main.js
+++ src/main.js
@@ -1,25 +1,64 @@
+import { WeatherService} from './weather-service.js';
+import { Giphy} from './weather-service.js';
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
     const city = $('#location').val();
     $('#location').val("");
 
-    let request = new XMLHttpRequest();
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+    let weatherService = new WeatherService()
+    let promise = weatherService.getWeatherByCity(city)// code moved to _weather-service.js_
 
-    request.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        getElements(response);
-        console.log(response.weather[0].description);
-      }
-    }
-
-    request.open("GET", url, true);
-    request.send();
-
-   const getElements = function(response) {
-      $('.showHumidity').text(`The humidity in ${city} is ${response.weather[0].description}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
-    }
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
+      $('.showTemp').text(`The temperature in temperature is ${body.main.temp} degrees.`);
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+    });
   });
+  $("#seattle").click(function(){
+    const seattle = $("#seattle").val()
+    let weatherForSeattle = new WeatherService()
+    let promise = weatherForSeattle.getWeatherByCity(seattle)
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      $('.showHumidity').text(`The humidity in ${seattle} is ${body.main.humidity}%`);
+      $('.showTemp').text(`The temperature in temperature is ${body.main.temp} degrees.`);
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+    });
+  })
+
+  $("#okay").click(function(){
+    const cityNames = ["Seattle","Portland","Arkansas"]
+    cityNames.forEach(function(cityN){
+      console.log(cityN)
+      let giphy1 = new Giphy();
+      let promise = giphy1.getGiphy(cityN);
+      console.log(promise)
+      promise.then(function(response){
+        let giphyBody = JSON.parse(response);
+        $("body").prepend(`<img src=${giphyBody.data[0].images.original.url}/>`)
+      }, function(error){
+        console.log(error)
+      })
+})
+  const cityIDArray = ["4720131","5809844","5812944","5814043"]
+  let newWeather = new WeatherService()
+  cityIDArray.forEach(function(city1){
+    let promise = newWeather.getWeatherByID(city1);
+    console.log(promise)
+    promise.then(function(response){
+      let cityBody = JSON.parse(response);
+      cityNames.push(cityBody.name)
+      console.log(cityNames)
+      $("body").append(`We will be updating ${cityBody.name}.`) ;
+    },
+    function(error){
+      console.log(error)
+      console.log(error.message)
+    }
+  )
+})
 });
+})
---
+++ src/weather-service.js
@@ -0,0 +1,53 @@
+export class WeatherService {
+  getWeatherByCity(city) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+  getWeatherByID(city1){
+    return new Promise(function(resolve,reject){
+      let request = new XMLHttpRequest();
+      const url = `http://api.openweathermap.org/data/2.5/weather?id=${city1}&APPID=4641ce3559b229171301671ee8722601`
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+
+    })
+  }
+}
+export class Giphy{
+      getGiphy(giphy1){
+        return new Promise(function(resolve,reject){
+          let request = new XMLHttpRequest();
+          const url = `http://api.giphy.com/v1/gifs/search?q=${giphy1}i&api_key=GarghQLp0bL3BhOfYvD0eLdP0TXGMzFD&limit=5`
+          request.onload = function(){
+            if(this.status === 200){
+              resolve(request.response)
+              console.log(this.status)
+            } else {
+              console.log(this.status)
+              reject(Error(request.statusText))
+
+            }
+          }
+          request.open("GET", url, true);
+          request.send();
+        });
+        }
+      }
